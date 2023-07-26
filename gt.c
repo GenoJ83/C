6decc626e372946f8881441f739c5aa29f25cee4
GenoJ83 <133131809+GenoJ83@users.noreply.github.com>
2023-07-26 12:22:43 +0300

    Completed

--- gt.c
+++ gt.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
     char region;
     printf("Enter your region(N,S,E,W,C):");
-    scanf("\n%c",region);
+    scanf("\n%c",&region);
 switch (region) {
 case 'N':
+printf("Emergency response initiated\n");
+break;
 case 'C':
 printf("Emergency response initiated\n");
 break;
 case 'E':
+printf("No emergency response required\n");
+break;
 case 'W':
+printf("No emergency response required\n");
+break;
 case 'S':
 printf("No emergency response required\n");
 break;
-
+default:
 }
 return 0;
 
